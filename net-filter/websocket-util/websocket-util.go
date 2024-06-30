06f28ccb00e5d4220faa9c0a4bf583710de193f1
peterq <me@peterq.cn>
2024-06-30 09:36:09 +0800

    feat: ws util ping/pong callback

--- net-filter/websocket-util/websocket-util.go
+++ net-filter/websocket-util/websocket-util.go
@@ -2,8 +2,9 @@ package websocket_util
 
 import (
 	"context"
+	"github.com/pkg/errors"
 	"golang.org/x/net/websocket"
-	"io/ioutil"
+	"io"
 	"log"
 )
 
@@ -13,7 +14,7 @@ func ReceiveFullFrame(ws *websocket.Conn, ctx context.Context) (byte, []byte, er
 }
 
 // ReceiveFullFramePingPongCb 接受完整帧
-func ReceiveFullFramePingPongCb(ws *websocket.Conn, ctx context.Context, pingPongCallback func(byte)) (byte, []byte, error) {
+func ReceiveFullFramePingPongCb(ws *websocket.Conn, ctx context.Context, pingPongCallback func(byte, []byte)) (byte, []byte, error) {
 	var data []byte
 	var pType byte
 	for {
@@ -34,8 +35,12 @@ func ReceiveFullFramePingPongCb(ws *websocket.Conn, ctx context.Context, pingPon
 	return pType, data, nil
 }
 
+type WritePong interface {
+	WritePong(msg []byte) (n int, err error)
+}
+
 // 接受帧
-func receiveFrame(cd websocket.Codec, ws *websocket.Conn, v interface{}, ctx context.Context, pingPongCb func(byte2 byte)) (payloadType byte, fin bool, err error) {
+func receiveFrame(cd websocket.Codec, ws *websocket.Conn, v interface{}, ctx context.Context, pingPongCb func(byte2 byte, data []byte)) (payloadType byte, fin bool, err error) {
 again:
 	if ctx.Err() != nil {
 		return payloadType, fin, ctx.Err()
@@ -50,8 +55,21 @@ again:
 		return
 	}
 	if frame.PayloadType() == websocket.PingFrame || frame.PayloadType() == websocket.PongFrame {
+		b := make([]byte, 1024)
+		var n int
+		n, err = io.ReadFull(frame, b)
+		if err != nil && err != io.EOF && err != io.ErrUnexpectedEOF {
+			return 0, false, errors.Wrap(err, "read ping pong error")
+		}
+		_, _ = io.Copy(io.Discard, frame)
+		if frame.PayloadType() == websocket.PingFrame {
+			var handler interface{} = ws
+			if _, err := handler.(WritePong).WritePong(b[:n]); err != nil {
+				return 0, false, errors.Wrap(err, "write pong error")
+			}
+		}
 		if pingPongCb != nil {
-			pingPongCb(frame.PayloadType())
+			pingPongCb(frame.PayloadType(), b[:n])
 		}
 	}
 	frame, err = ws.HandleFrame(frame)
@@ -64,7 +82,7 @@ again:
 
 	payloadType = frame.PayloadType()
 
-	data, err := ioutil.ReadAll(frame)
+	data, err := io.ReadAll(frame)
 	if err != nil {
 		return
 	}
